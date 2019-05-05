e17e5afa535cc22c1e358a6c76794fcfc93fc592
Daniel Harrington <danieledwardharrington@gmail.com>
2019-05-05 12:47:54 -0400

    added all kinds of stuff again

--- app/src/main/java/com/abc/danielharrington/betterdays/AboutFragment.java
+++ app/src/main/java/com/abc/danielharrington/betterdays/AboutFragment.java
@@ -3,15 +3,10 @@ package com.abc.danielharrington.betterdays;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 
 public class AboutFragment extends Fragment {
 
